084bb85af00730833e6d39f03779d577a4426f63
467980 <benedickcorpuz274@gmail.com>
2025-07-04 22:58:10 +0800

    Create evenNumbers.js

---
+++ evenNumbers.js
@@ -0,0 +1,6 @@
+// use for loop its because you know how many you gonna loop
+for (let i = 1; i <= 10; i++) {//let i stat at 1 end at 10
+    if (i % 2 === 0) {//use modulo to start the for loop at 3 === to if the same value
+        console.log(i);
+    }
+}
