257b50e09d259288e55e7644ea6288e3074846b6
467980 <benedickcorpuz274@gmail.com>
2025-07-04 22:26:49 +0800

    Update and rename README.md to gradeAsssignment.js

---
+++ gradeAsssignment.js
@@ -0,0 +1,13 @@
+let score = prompt("Enter your score:");
+//let for variable and prompt agian for browser dialog
+score = Number(score);
+
+if (score >= 90) {//use if here to one condition
+    console.log("Your grade is: Excellent.");
+} else if (score >= 80) {//else if of multiple condition
+    console.log("Your grade is: Good.");
+} else if (score >= 70) {//so the user input use (89) false next condition is true its stop the condition
+    console.log("Your grade is: Fair.");
+} else {
+    console.log("Your grade is: Needs Improvement.");
+}//in esle when all off that false it gonna direct here if no else put here there is no out put goons show
